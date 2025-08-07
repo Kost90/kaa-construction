5fe94e90767e76ed36ab5c0139fdea131200569b
Ivanus Volodymyr <ivanys230796@gmail.com>
2025-08-07 19:38:03 +0100

    - added favico icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,31 +6,32 @@ module.exports = {
     title: "KAA Construction",
   },
   plugins: [
-  "gatsby-plugin-image",
-  "gatsby-plugin-sharp",
-  `gatsby-plugin-mdx`,
-  "gatsby-transformer-sharp",
-  {
-    resolve: "gatsby-source-filesystem",
-    options: {
-      name: `blog`,
-      path: `${__dirname}/blog`,
-    }
-  },
-  {
-    resolve: "gatsby-source-filesystem",
-    options: {
-      name: `data`,
-      path: `${__dirname}/data`,
-    }
-  },
-  {
-    resolve: 'gatsby-plugin-react-svg',
-    options: {
-      rule: {
-        include: /assets/
-      }
-    }
-  }
-],
-}
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    `gatsby-plugin-mdx`,
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `data`,
+        path: `${__dirname}/data`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
+};
